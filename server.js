78b5cd74727bcd34c23c47fe6ef6e8b283da9c70
danpayne7 <dp883036@gmail.com>
2026-02-28 17:03:29 +0000

    express

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use((req,res,next) => {
+  //logs every request
+  console.log(`${req.method} ${req.url} - ${new Date()}`);
+  next(); //pass to next handler (required)
+});
+ 
+app.get('/search', (req, res) => {
+  const id = req.query.id;
+  console.log(id);
+  res.send(id);
+});
+
+app.get("/",(req,res) => {
+  res.send('hello')
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+});
